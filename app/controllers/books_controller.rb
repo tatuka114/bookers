cba37e8441bba57a4becf3527564a3bf22ff317f
tatuka114 <matsuryu0214@gmail.com>
2021-05-25 14:14:38 +0000

    [Update]3-8 index_error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,20 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    @book = Book.new
+    @book = Book.new(book_params)
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book)
+    else
+    render :new
+    end
   end
 
   def edit
@@ -23,14 +22,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
 
